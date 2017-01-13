3d14c263db4abc233fabb4f3809419797256c7c0
minibrary <minibrary@gmail.com>
2017-01-13 20:35:17 +0900

    what if

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -57,12 +57,13 @@
     <div class="clearfix"></div>
 
 <!-- TABLE START -->
+     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
             <h2>My Note <small>Domains</small></h2>
             <div class="clearfix"></div>
-          </div>
+	  </div>
           <div class="x_content">
             <p class="text-muted font-13 m-b-30">
               The Buttons extension for DataTables provides a common set of options, API methods and styling to display buttons on a page that will interact with a DataTable. The core library provides the based framework upon which plug-ins can built.
@@ -113,34 +114,34 @@
 </div>
 
 <!-- jQuery -->
-<script src="gentellela/vendors/jquery/dist/jquery.min.js"></script>
+<script src="vendors/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="gentellela/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- FastClick -->
-<script src="gentellela/vendors/fastclick/lib/fastclick.js"></script>
+<script src="vendors/fastclick/lib/fastclick.js"></script>
 <!-- NProgress -->
-<script src="gentellela/vendors/nprogress/nprogress.js"></script>
+<script src="vendors/nprogress/nprogress.js"></script>
 <!-- iCheck -->
-<script src="gentellela/vendors/iCheck/icheck.min.js"></script>
+<script src="vendors/iCheck/icheck.min.js"></script>
 <!-- Datatables -->
-<script src="gentellela/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="gentellela/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<script src="gentellela/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="gentellela/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-<script src="gentellela/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-<script src="gentellela/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="gentellela/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="gentellela/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-<script src="gentellela/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-<script src="gentellela/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="gentellela/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-<script src="gentellela/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
-<script src="gentellela/vendors/jszip/dist/jszip.min.js"></script>
-<script src="gentellela/vendors/pdfmake/build/pdfmake.min.js"></script>
-<script src="gentellela/vendors/pdfmake/build/vfs_fonts.js"></script>
+<script src="vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+<script src="vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+<script src="vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+<script src="vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+<script src="vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+<script src="vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+<script src="vendors/jszip/dist/jszip.min.js"></script>
+<script src="vendors/pdfmake/build/pdfmake.min.js"></script>
+<script src="vendors/pdfmake/build/vfs_fonts.js"></script>
 
 <!-- Custom Theme Scripts -->
-<script src="gentellela/build/js/custom.min.js"></script>
+<script src="js/custom.min.js"></script>
 
 <!-- Datatables -->
 <script>
